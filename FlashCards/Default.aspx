b2716142a832c44aff1897905f64abc392896e0b
robertjgraves <robertjgraves@gmail.com>
2015-05-31 12:51:19 -0700

    Initial commit

---
+++ FlashCards/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FlashCards.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Multiplication Flash Cards</h1>
+        <p>
+            <asp:Label ID="multiplicandLabel" runat="server" Text=""></asp:Label> &times; 
+            <asp:Label ID="multiplierLabel" runat="server" Text=""></asp:Label> = 
+            <asp:TextBox ID="answerTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+           <asp:Button ID="checkAnswerButton" runat="server" Text="Check Answer" OnClick="checkAnswerButton_Click" />
+           <asp:Button ID="newCard" runat="server" Text="Get New Card" OnClick="newCard_Click" />
+        </p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server" Text=""></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="streakLabel" runat="server" Text="Streak: "></asp:Label>
+            <asp:Label ID="countLabel" runat="server" Text="0"></asp:Label>
+        </p>
+
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ FlashCards/Default.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FlashCards
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        Random rand = new Random();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                setupCards();
+            }
+           
+        }
+
+        protected void checkAnswerButton_Click(object sender, EventArgs e)
+        {
+            if(answerTextBox.Text != "")
+            {
+                int product = int.Parse(multiplicandLabel.Text) * int.Parse(multiplierLabel.Text);
+                int answer = int.Parse(answerTextBox.Text);
+
+                checkAnswer(product, answer);
+            }
+            
+            else
+            {
+                resultLabel.Text = "Please enter a value.";
+            }
+        }
+
+        public void setupCards()
+        {
+            int startingValue = 2;
+            int endingValue = 10;
+
+            int multiplicand = getNumber(startingValue, endingValue);
+            int multiplier = getNumber(startingValue, endingValue);
+
+            multiplicandLabel.Text = multiplicand.ToString();
+            multiplierLabel.Text = multiplier.ToString();
+            answerTextBox.Text = "";
+        }
+
+        public int getNumber(int start, int end)
+        {
+            return rand.Next(start, end);
+        }
+
+        public void checkAnswer(int product, int answer)
+        {
+            if (product == answer)
+            {
+                resultLabel.Text = "You are correct!";
+                int streak = int.Parse(countLabel.Text);
+                streak++;
+                countLabel.Text = streak.ToString();
+            }
+            else
+            { 
+                resultLabel.Text = String.Format("This answer is {0}.", product);
+                countLabel.Text = "0";
+            }
+        }
+
+        protected void newCard_Click(object sender, EventArgs e)
+        {
+            setupCards();
+        }
+    }
+}
